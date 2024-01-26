9b30c35dfd88d46d4555b61c7d5bf604559851c0
Mustafa07 <151774871+Mustafa141207@users.noreply.github.com>
2024-01-26 21:53:03 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+    let sideBarWidth = $("#innerSide").innerWidth();
+     $("#sideBar").css("left", -sideBarWidth);
+    $('#sideBar i').click(function () {
+        if ($("#sideBar").css("left")== '0px') {
+            $('#sideBar').animate({ left: -sideBarWidth }, 100)
+        } else {
+            console.log('hi')
+             $("#sideBar").animate({ left:0 }, 100);
+        }
+    })
+
+    $('#party h5').click(function (e) {
+        let x = (e.target);
+        if ($(x).next(".inner").css('display') == 'block') {
+            $(x).next(".inner").css("display", "none");
+        } else {
+             $(x).next(".inner").css("display", "block");
+        }
+        
+            
+            
+    })
+    $('#innerSide').click(function (e) {
+        let href = $(e.target).attr('href');
+        let scrool=$(href).offset().top
+        $("body , html ").animate({ scrollTop: scrool }, 800)
+
+    })
+ 
+        
+})
+document.querySelector('#box').addEventListener('keyup', function (e) {
+    console.log(e.key)
+     let x = $("#counter").html();
+    if (e.key == 'Backspace'&x<100) {
+           x++
+           $("#counter").html(x);
+    } else if(x>=1) {
+          let z = x - 1;
+          $("#counter").html(z);
+    }
+ 
+  
+   
+
+  
+   
+   
+
+
+    
+   
+ 
+})
